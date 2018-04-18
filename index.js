43a12454194bfcedff8ccb8f6bbdbbb69f240197
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-04-18 18:26:49 +0200

    cpmv

---
+++ index.js
@@ -0,0 +1,55 @@
+var fs = require('fs')
+var pump = require('pump')
+var pumpDir = require('pump-dir')
+var rimraf = require('rimraf')
+
+function stat (filepath, opts, cb) {
+  opts && opts.dereference ? fs.stat(filepath, cb) : fs.lstat(filepath, cb)
+}
+
+function cp (from, to, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+
+  stat(from, opts, function (err, stats) {
+    if (err) return cb(err)
+
+    var pumper
+    if (stats.isDirectory()) {
+      pumper = pumpDir
+    } else if (stats.isFile()) {
+      pumper = pump
+      from = fs.createReadStream(from)
+      to = fs.createWriteStream(to)
+    } else {
+      return cb(Error('unsupported resource'))
+    }
+
+    pumper(from, to, function (err) {
+      if (err) return cb(err)
+      cb(null)
+    })
+
+  })
+}
+
+function mv (from, to, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+  cp(from, to, opts, function oncopied (err) {
+    if (err) return cb(err)
+    rimraf(from, function onrimraf (err) {
+      if (err) return cb(err)
+      cb(null)
+    })
+  })
+}
+
+module.exports = {
+  cp: cp,
+  mv: mv
+}
