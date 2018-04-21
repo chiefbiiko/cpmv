cc03ce42aab31636cffe9a67b27b87527b80b4d0
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-04-21 10:39:55 +0200

    tests

---
+++ test.js
@@ -0,0 +1,29 @@
+var tape = require('tape')
+var { cp, mv } = require('./index')
+var { existsSync, unlink, writeFile } = require('fs')
+
+tape.onFinish(function () {
+  unlink(__filename + '_copy', function (_) {
+    unlink('node_modules/mv.test', function (_) {})
+  })
+})
+
+tape('cp', function (t) {
+  cp(__filename, __filename + '_copy', function (err) {
+    if (err) return t.end(err)
+    t.ok(existsSync(__filename + '_copy'), 'copied')
+    t.end()
+  })
+})
+
+tape('mv', function (t) {
+  writeFile('mv.test', '419', function (err) {
+    if (err) t.end(err)
+    mv('mv.test', 'node_modules/mv.test', function (err) {
+      if (err) return t.end(err)
+      t.ok(existsSync('node_modules/mv.test'), 'moved')
+      t.notOk(existsSync('mv.test'), 'gone')
+      t.end()
+    })
+  })
+})
